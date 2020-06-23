da73dbd53ce0ee8f26e0ea12d9b3dfdc175d272e
gabybenedicta <gaby.benedicta@yahoo.com>
2020-06-23 16:48:58 +0800

    Create a middleware to check for token in Authorization field in the header

--- vav/settings.py
+++ vav/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'mockplatform.middleware.VerifyTokenMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
