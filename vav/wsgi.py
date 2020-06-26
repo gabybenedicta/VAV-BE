116d5a11bc6c21c4f90a8ab3c2a43f311c7057fa
khanh <khanh.phungntu@gmail.com>
2020-06-26 23:41:30 +0800

    Fix credential path

--- vav/wsgi.py
+++ vav/wsgi.py
@@ -13,9 +13,10 @@ import json
 from django.core.wsgi import get_wsgi_application
 import firebase_admin
 from firebase_admin import credentials
+from os.path import abspath
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vav.settings")
-cred = credentials.Certificate("D:\disk_D\VISA 2020\Visa Google Cloud\VAV-BE\credentials.json")
+cred = credentials.Certificate(abspath("./credentials.json"))
 
 default_app = firebase_admin.initialize_app(cred)
 application = get_wsgi_application()
